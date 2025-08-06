45fca082f85b6b90406b67f2f0b3c0ede2bc648b
mandar4415 <mandarnikam9@gmail.com>
2025-08-06 17:53:00 +0530

    backend: add models (User, Task, TimeLog)

---
+++ backend/models/TimeLog.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const timeLogSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  task: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task',
+    required: true
+  },
+  startTime: {
+    type: Date,
+    required: true
+  },
+  endTime: {
+    type: Date
+  },
+  duration: {
+    type: Number // duration in seconds
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('TimeLog', timeLogSchema);
